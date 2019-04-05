c4de0a7cc7465ba7a6b0cf0497a4637c6518a0c6
Anton Khodak <anton.khodak@ukr.net>
2019-04-05 14:05:30 +0100

    Upgrade upgrade commands

--- sanger/jupyter/large-upgrade.sh
+++ sanger/jupyter/large-upgrade.sh
@@ -2,5 +2,5 @@
 # upgrades jupyter large
 kubectl config use-context large-cellgeni
 ssh -i ~/.ssh/farm4-head1-id_rsa -o ServerAliveInterval=5 -o ServerAliveCountMax=1 -l ubuntu -Nf -L 16700:10.0.3.15:6443 172.27.18.144
-helm upgrade jptl jupyterhub/jupyterhub --namespace jptl --version 0.7.0 --values jupyter-large-config.yaml
+helmold upgrade jptl jupyterhub/jupyterhub --namespace jptl --version 0.7.0 --values jupyter-large-config.yaml # helmold is a custom command for helm ver. 2.9.1
 # helm upgrade --install jptl jupyterhub/jupyterhub --namespace jptl --version 0.7.0 --values jupyter-large-config.yaml
