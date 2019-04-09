6bace0f416bb75ec821455bf9a8df44abcc19695
Anton Khodak <anton.khodak@ukr.net>
2019-04-09 13:25:33 +0100

    New ingress rules

--- sanger/jupyter/large-upgrade.sh
+++ sanger/jupyter/large-upgrade.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # upgrades jupyter large
 kubectl config use-context eta
-ssh -i ~/.ssh/farm4-head1-id_rsa -o ServerAliveInterval=5 -o ServerAliveCountMax=1 -l ubuntu -Nf -L 16900:10.0.3.15:6443 172.27.18.144
 helm upgrade jptl jupyterhub/jupyterhub --namespace jptl --version 0.8.0 --values jupyter-large-config.yaml
 # helm upgrade --install jptl jupyterhub/jupyterhub --namespace jptl --version 0.8.0 --values jupyter-large-config.yaml
