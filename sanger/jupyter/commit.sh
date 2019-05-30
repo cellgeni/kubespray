3bcf73d7688c3df7da4fdebe6315ed33808b1e23
Anton Khodak <anton.khodak@ukr.net>
2019-05-30 15:35:50 +0100

    Deployment for kidneycellatlas

--- sanger/jupyter/commit.sh
+++ sanger/jupyter/commit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /Users/ak27/programming/cellgeni/kubespray/sanger/sites
-git add jupyter-github-auth.yaml jupyter-large-config.yaml && git commit -m "Add new users" && git push
+git add jupyter-github-auth.yaml jupyter-large-config.yaml jupyter-xl.yaml jupyter-test.yaml && git commit -m "Add new users" && git push
 cd -
