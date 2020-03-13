65617956dd4b888e0582e01477c357c76ddbc832
Anton Khodak <ak27@sanger.ac.uk>
2020-03-13 14:08:44 +0000

    Fix fetal-skin-lymphoid-T-NK 404 error

--- sanger/jupyter/github-upgrade.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# upgrades newest jupyter installation
-kubectl config use-context eta
-helm upgrade jpt jupyterhub/jupyterhub --namespace jpt --version 0.8.0 --values jupyter-github-auth.yaml
